79853730e39d4896e13868e43de7cfa6438c08d7
Miles Matthias <mmatthias@google.com>
2019-11-06 18:02:38 -0700

    move view generator to its own test

--- test/fixtures/view_generator/main.tf
+++ test/fixtures/view_generator/main.tf
@@ -21,15 +21,12 @@ provider "google" {
   version = "~> 2.5.0"
 }
 
-module "bigquery" {
-  source            = "../.."
-  dataset_id        = "foo"
-  dataset_name      = "foo"
-  description       = "some description"
+module "example" {
+  source            = "../../../examples/multiple_tables"
   expiration        = var.expiration
   project_id        = var.project_id
-  location          = "US"
   tables            = var.tables
   time_partitioning = var.time_partitioning
   dataset_labels    = var.dataset_labels
 }
+
