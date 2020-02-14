5b6f758f80b14299ac28e664a15d3b5e2b5eea5b
Umair Idris <umairidris@google.com>
2020-02-14 05:33:42 +0000

    cleanup submodule output

--- modules/udf/outputs.tf
+++ modules/udf/outputs.tf
@@ -16,5 +16,5 @@
 
 output "added_udfs" {
   description = "List of UDFs utility functions added."
-  value       = var.add_udfs ? ["find_in_set", "check_protocol", "parse_url", "csv_to_struct"] : []
+  value       = ["find_in_set", "check_protocol", "parse_url", "csv_to_struct"]
 }
