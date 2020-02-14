68b80a6ef700fa1379bd5c02e8314a7c19fefd2b
Umair Idris <umairidris@google.com>
2020-02-14 05:06:09 +0000

    set types on vars and make optional ones actually optional

--- variables.tf
+++ variables.tf
@@ -16,33 +16,42 @@
 
 variable "dataset_id" {
   description = "Unique ID for the dataset being provisioned."
+  type        = string
 }
 
 variable "dataset_name" {
   description = "Friendly name for the dataset being provisioned."
+  type        = string
+  default     = null
 }
 
 variable "description" {
   description = "Dataset description."
+  type        = string
+  default     = null
 }
 
 variable "location" {
   description = "The regional location for the dataset only US and EU are allowed in module"
+  type        = string
   default     = "US"
 }
 
 variable "default_table_expiration_ms" {
   description = "TTL of tables using the dataset in MS"
+  type        = number
   default     = null
 }
 
 variable "project_id" {
   description = "Project where the dataset and table are created"
+  type        = string
 }
 
 variable "dataset_labels" {
   description = "Key value pairs in a map for dataset labels"
   type        = map(string)
+  default     = null
 }
 
 variable "tables" {
