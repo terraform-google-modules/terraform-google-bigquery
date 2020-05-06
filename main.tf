92cd526dbbbd516a432a301fadb25d6e5e448c33
Morgante Pell <morgante.pell@morgante.net>
2020-05-06 18:54:10 -0400

    Revert "fix: Map dataEditor to WRITER role"

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ locals {
   tables = { for table in var.tables : table["table_id"] => table }
   iam_to_primitive = {
     "roles/bigquery.dataOwner" : "OWNER"
-    "roles/bigquery.dataEditor" : "WRITER"
+    "roles/bigquery.dataEditor" : "EDITOR"
     "roles/bigquery.dataViewer" : "READER"
   }
 }
