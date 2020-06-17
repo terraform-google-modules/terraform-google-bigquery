934996e80a79c5fa2743916ba0b1f3a0fe17f160
Morgante Pell <morgantep@google.com>
2020-06-17 16:23:49 -0400

    Simplify basic example

--- examples/basic_bq/main.tf
+++ examples/basic_bq/main.tf
@@ -15,12 +15,29 @@
  */
 
 module "bigquery" {
-  source         = "../.."
-  dataset_id     = "foo"
-  dataset_name   = "foo"
-  description    = "some description"
-  project_id     = var.project_id
-  location       = "US"
-  tables         = var.tables
-  dataset_labels = var.dataset_labels
+  source       = "../.."
+  dataset_id   = "foo"
+  dataset_name = "foo"
+  description  = "some description"
+  project_id   = var.project_id
+  location     = "US"
+  tables = [
+    {
+      table_id          = "bar",
+      schema            = "sample_bq_schema.json",
+      time_partitioning = null,
+      expiration_time   = 2524604400000, # 2050/01/01
+      clustering        = [],
+      labels = {
+        env      = "devops"
+        billable = "true"
+        owner    = "joedoe"
+      },
+    }
+  ]
+  dataset_labels = {
+    env      = "dev"
+    billable = "true"
+    owner    = "janesmith"
+  }
 }
