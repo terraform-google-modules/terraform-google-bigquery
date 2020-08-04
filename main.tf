c2a2ea5e860c15a273d3e3bf8c0126a51b8422e5
camillemch <ca.michel@lvmh.fr>
2020-08-04 19:57:20 +0200

    Remove access fix

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ locals {
   tables = { for table in var.tables : table["table_id"] => table }
   views  = { for view in var.views : view["view_id"] => view }
 
-  iam_to_primitive = {
-    "roles/bigquery.dataOwner" : "OWNER"
-    "roles/bigquery.dataEditor" : "WRITER"
-    "roles/bigquery.dataViewer" : "READER"
-  }
+  # iam_to_primitive = {
+  #   "roles/bigquery.dataOwner" : "OWNER"
+  #   "roles/bigquery.dataEditor" : "WRITER"
+  #   "roles/bigquery.dataViewer" : "READER"
+  # }
 }
 
 resource "google_bigquery_dataset" "main" {
@@ -35,20 +35,20 @@ resource "google_bigquery_dataset" "main" {
   project                     = var.project_id
   labels                      = var.dataset_labels
 
-  dynamic "access" {
-    for_each = var.access
-    content {
-      # BigQuery API converts IAM to primitive roles in its backend.
-      # This causes Terraform to show a diff on every plan that uses IAM equivalent roles.
-      # Thus, do the conversion between IAM to primitive role here to prevent the diff.
-      role = lookup(local.iam_to_primitive, access.value.role, access.value.role)
+  # dynamic "access" {
+  #   for_each = var.access
+  #   content {
+  #     # BigQuery API converts IAM to primitive roles in its backend.
+  #     # This causes Terraform to show a diff on every plan that uses IAM equivalent roles.
+  #     # Thus, do the conversion between IAM to primitive role here to prevent the diff.
+  #     role = lookup(local.iam_to_primitive, access.value.role, access.value.role)
 
-      domain         = lookup(access.value, "domain", null)
-      group_by_email = lookup(access.value, "group_by_email", null)
-      user_by_email  = lookup(access.value, "user_by_email", null)
-      special_group  = lookup(access.value, "special_group", null)
-    }
-  }
+  #     domain         = lookup(access.value, "domain", null)
+  #     group_by_email = lookup(access.value, "group_by_email", null)
+  #     user_by_email  = lookup(access.value, "user_by_email", null)
+  #     special_group  = lookup(access.value, "special_group", null)
+  #   }
+  # }
 }
 
 resource "google_bigquery_table" "main" {
