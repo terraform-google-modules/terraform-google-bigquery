5621455340ab670376f21f14bdcdce6cccaec7c0
Thiago Carvalho <thiagonbcarvalho@gmail.com>
2020-11-10 22:55:42 -0300

    Skipes gcloud download

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -18,11 +18,12 @@ module "project" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 9.0"
 
-  name              = "ci-bigquery"
-  random_project_id = "true"
-  org_id            = var.org_id
-  folder_id         = var.folder_id
-  billing_account   = var.billing_account
+  name                 = "ci-bigquery"
+  random_project_id    = "true"
+  org_id               = var.org_id
+  folder_id            = var.folder_id
+  billing_account      = var.billing_account
+  skip_gcloud_download = true
 
   activate_apis = [
     "cloudresourcemanager.googleapis.com",
