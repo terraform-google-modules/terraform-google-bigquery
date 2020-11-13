01c6e0ba3344ec0958406dbbaec679f8a1bccd3d
kpedersen <kpedersen@pythian.com>
2020-11-13 12:04:31 -0500

    removing explicit dependency

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -51,7 +51,6 @@ module "initialize_encryption_account" {
   #pinned to prevent bug with random cache count evaluation, see #82
   source     = "terraform-google-modules/gcloud/google"
   version    = "1.2.0"
-  depends_on = [module.project] #waits for API config
 
   platform              = "linux"
   additional_components = ["bq"]
