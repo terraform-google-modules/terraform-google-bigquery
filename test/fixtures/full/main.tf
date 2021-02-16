20466e0437e8d737e54e67de247967879e711edf
LCStephens <lcstephens@utexas.edu>
2021-02-16 11:21:15 -0600

    remove unnecessary external_tables variable

--- test/fixtures/full/main.tf
+++ test/fixtures/full/main.tf
@@ -19,7 +19,6 @@ module "example" {
   default_table_expiration_ms = var.default_table_expiration_ms
   project_id                  = var.project_id
   tables                      = var.tables
-  external_tables             = var.external_tables
   dataset_labels              = var.dataset_labels
   kms_key                     = jsondecode(var.kms_keys)["foo"]
 }
