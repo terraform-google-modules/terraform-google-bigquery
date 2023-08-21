dd0e1589e376f41a7161c36bba684275323296d3
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-08-21 16:30:11 -0400

    initial changes

--- modules/data_warehouse/src/sql/sp_bigqueryml_model.sql
+++ modules/data_warehouse/src/sql/sp_bigqueryml_model.sql
@@ -12,28 +12,58 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-/* Run a query to see the prediction results of the model
+/*
+Run a query to see the results of the model
 --
-select * from ML.PREDICT(MODEL ds_edw.model_taxi_estimate,
-  TABLE ds_edw.taxi_trips)
-  limit 1000;  */
+SELECT
+  CONCAT('cluster ', CAST(centroid_id as STRING)) as cluster,
+  avg_spend as average_spend,
+  count_orders as count_of_orders,
+  days_since_order
+FROM (
+  SELECT
+    centroid_id,
+    feature,
+    ROUND(numerical_value, 2) as value
+  FROM
+    ML.CENTROIDS(MODEL `ds_edw.customer_segment_clustering`)
+)
+PIVOT (
+  SUM(value)
+  FOR feature IN ('avg_spend',  'count_orders', 'days_since_order')
+)
+ORDER BY centroid_id
+*/
 
 --Model Example
-CREATE OR REPLACE MODEL
-  `${project_id}.ds_edw.model_taxi_estimate` OPTIONS ( MODEL_TYPE='LINEAR_REG',
-    LS_INIT_LEARN_RATE=0.15,
-    L1_REG=1,
-    MAX_ITERATIONS=5 ) AS
-SELECT
-  pickup_datetime,
-  dropoff_datetime,
-  IFNULL(passenger_count,0) passenger_count,
-  IFNULL(trip_distance,0) trip_distance,
-  IFNULL(rate_code,'') rate_code,
-  IFNULL(payment_type,'') payment_type,
-  IFNULL(fare_amount,0) label,
-  IFNULL(pickup_location_id,'') pickup_location_id
-FROM
-  `${project_id}.ds_edw.taxi_trips`
-WHERE
-  fare_amount > 0;
+CREATE OR REPLACE MODEL `${project_id}.ds_edw.customer_segment_clustering`
+OPTIONS(
+  MODEL_TYPE = 'KMEANS', -- model name
+  NUM_CLUSTERS = 5, -- how many clusters to create
+  KMEANS_INIT_METHOD = 'KMEANS++',
+  STANDARDIZE_FEATURES = TRUE -- note: normalization taking place to scale the range of independent variables (each feature contributes proportionately to the final distance)
+)
+AS (
+  SELECT
+    * EXCEPT (user_id)
+  FROM (
+    SELECT
+      user_id,
+      DATE_DIFF(CURRENT_DATE(), CAST(MAX(order_created_date) as DATE), day) as days_since_order, ---RECENCY
+      COUNT(order_id) as count_orders, --FREQUENCY
+      AVG(sale_price) as avg_spend --MONETARY
+    FROM (
+      SELECT
+        user_id,
+        order_id,
+        sale_price,
+        created_at as order_created_date
+      FROM
+        `${project_id}.ds_edw.order_items`
+      WHERE
+        created_at BETWEEN TIMESTAMP('2020-07-31 00:00:00')
+        AND TIMESTAMP('2023-07-31 00:00:00')
+    )
+    GROUP BY user_id
+  )
+);
