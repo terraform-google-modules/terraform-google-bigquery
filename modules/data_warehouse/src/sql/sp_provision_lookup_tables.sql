98e55e4f8248d08a9f8ce88d7b5901be1917257a
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-09-26 12:32:31 -0400

    Correct lookup tables query

--- modules/data_warehouse/src/sql/sp_provision_lookup_tables.sql
+++ modules/data_warehouse/src/sql/sp_provision_lookup_tables.sql
@@ -21,22 +21,23 @@ CREATE OR REPLACE TABLE `${project_id}.ds_edw.distribution_centers`
         distribution_center_geom GEOGRAPHY
     )
 AS
-SELECT 1, 'Memphis TN', 35.1174, -89.9711, ST_GEOGPOINT(longitude, latitude)
+SELECT 1, 'Memphis TN', 35.1174, -89.9711, ST_GEOGPOINT(35.1174, -89.9711)
 UNION ALL
-SELECT 2, 'Chicago IL', 41.8369, -87.6847, ST_GEOGPOINT(longitude, latitude)
+SELECT 2, 'Chicago IL', 41.8369, -87.6847, ST_GEOGPOINT(41.8369, -87.6847)
 UNION ALL
-SELECT 3, 'Houston TX', 29.7604, -95.3698, ST_GEOGPOINT(longitude, latitude)
+SELECT 3, 'Houston TX', 29.7604, -95.3698, ST_GEOGPOINT(29.7604, -95.3698)
 UNION ALL
-SELECT 4, 'Los Angeles CA', 34.05, -118.25, ST_GEOGPOINT(longitude, latitude)
+SELECT 4, 'Los Angeles CA', 34.05, -118.25, ST_GEOGPOINT(34.05, -118.25)
 UNION ALL
-SELECT 5, 'New Orleans LA', 29.95, -90.0667, ST_GEOGPOINT(longitude, latitude)
+SELECT 5, 'New Orleans LA', 29.95, -90.0667, ST_GEOGPOINT(29.95, -90.0667)
 UNION ALL
-SELECT 6, 'Port Authority of New York/New Jersey NY/NJ', 40.634, -73.7834, ST_GEOGPOINT(longitude, latitude)
+SELECT 6, 'Port Authority of New York/New Jersey NY/NJ', 40.634, -73.7834, ST_GEOGPOINT(40.634, -73.7834)
 UNION ALL
-SELECT 7, 'Philadelphia PA', 39.95, -75.1667, ST_GEOGPOINT(longitude, latitude)
+SELECT 7, 'Philadelphia PA', 39.95, -75.1667, ST_GEOGPOINT(39.95, -75.1667)
 UNION ALL
-SELECT 8, 'Mobile AL', 30.6944, -88.0431, ST_GEOGPOINT(longitude, latitude)
+SELECT 8, 'Mobile AL', 30.6944, -88.0431, ST_GEOGPOINT(30.6944, -88.0431)
 UNION ALL
-SELECT 9, 'Charleston SC', 32.7833, -79.9333, ST_GEOGPOINT(longitude, latitude)
+SELECT 9, 'Charleston SC', 32.7833, -79.9333, ST_GEOGPOINT(32.7833, -79.9333)
 UNION ALL
-SELECT 10, 'Savannah GA', 32.0167, -81.1167, ST_GEOGPOINT(longitude, latitude);
+SELECT 10, 'Savannah GA', 32.0167, -81.1167, ST_GEOGPOINT(32.0167, -81.1167)
+;
