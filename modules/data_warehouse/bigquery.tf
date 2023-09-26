765fa969ee179ed9780bd050b44df151d4336bbb
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-09-26 16:56:30 -0400

    Staleness testing

--- modules/data_warehouse/bigquery.tf
+++ modules/data_warehouse/bigquery.tf
@@ -81,7 +81,7 @@ resource "google_bigquery_table" "tbl_edw_inventory_items" {
   table_id            = "inventory_items"
   project             = module.project-services.project_id
   deletion_protection = var.deletion_protection
-  max_staleness =  "1:0:0"
+  # max_staleness =  "1:0:0"
 
   schema = file("${path.module}/src/schema/inventory_items_schema.json")
 
@@ -90,7 +90,7 @@ resource "google_bigquery_table" "tbl_edw_inventory_items" {
     connection_id = google_bigquery_connection.ds_connection.name
     source_format = "PARQUET"
     source_uris   = ["gs://${google_storage_bucket.raw_bucket.name}/thelook-ecommerce/inventory_items.parquet"]
-    metadata_cache_mode = "AUTOMATIC"
+    # metadata_cache_mode = "AUTOMATIC"
   }
 
   depends_on = [
