165b41f70d99424d28a81e6f3005e974025be331
Awais Malik <awmalik@google.com>
2023-10-06 16:08:16 -0700

    chore: upgraded versions.tf to include minor bumps from tpg v5

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.53.0, < 5.0"
+      version = ">= 3.53.0, < 6"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.53.0, < 5.0"
+      version = ">= 3.53.0, < 6"
     }
   }
 }
