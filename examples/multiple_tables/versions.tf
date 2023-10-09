890d511ec8c8ecc837c3227432d2ee1f61f39f2c
Andrew Peabody <andrewpeabody@google.com>
2023-10-09 11:09:54 -0700

    Update versions.tf

--- examples/multiple_tables/versions.tf
+++ examples/multiple_tables/versions.tf
@@ -17,8 +17,7 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
+      source = "hashicorp/google"
     }
   }
   required_version = ">= 0.13"
