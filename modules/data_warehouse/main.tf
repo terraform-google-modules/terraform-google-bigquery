fa2b0dae510c8d95250b085390085ebf3ae3a82d
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-11-09 11:51:51 -0500

    Formatting

--- modules/data_warehouse/main.tf
+++ modules/data_warehouse/main.tf
@@ -37,8 +37,6 @@ module "project-services" {
     "config.googleapis.com",
     "datacatalog.googleapis.com",
     "datalineage.googleapis.com",
-    # "eventarc.googleapis.com",
-    # "pubsub.googleapis.com",
     "serviceusage.googleapis.com",
     "storage.googleapis.com",
     "storage-api.googleapis.com",
@@ -46,12 +44,6 @@ module "project-services" {
   ]
 
   activate_api_identities = [
-    # {
-      # api = "pubsub.googleapis.com"
-      # roles = [
-      #   "roles/iam.serviceAccountTokenCreator",
-    #   ]
-    # },
     {
       api = "workflows.googleapis.com"
       roles = [
@@ -66,13 +58,12 @@ resource "time_sleep" "wait_after_apis" {
   depends_on      = [module.project-services]
 }
 
-// Create random ID to be used for deployment uniqueness
+## Create random ID to be used for deployment uniqueness
 resource "random_id" "id" {
   byte_length = 4
 }
 
 # Set up Storage Buckets
-
 # # Set up the raw storage bucket
 resource "google_storage_bucket" "raw_bucket" {
   name                        = "ds-edw-raw-${random_id.id.hex}"
@@ -103,24 +94,6 @@ resource "google_storage_bucket" "provisioning_bucket" {
   labels = var.labels
 }
 
-# // Create Eventarc Trigger
-# # # Create a Pub/Sub topic.
-# resource "google_pubsub_topic" "topic" {
-#   name    = "provisioning-topic"
-#   project = module.project-services.project_id
-
-#   depends_on = [time_sleep.wait_after_apis]
-
-#   labels = var.labels
-# }
-
-# resource "google_pubsub_topic_iam_binding" "binding" {
-#   project = module.project-services.project_id
-#   topic   = google_pubsub_topic.topic.id
-#   role    = "roles/pubsub.publisher"
-#   members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
-# }
-
 # # Get the GCS service account to trigger the pub/sub notification
 data "google_storage_project_service_account" "gcs_account" {
   project = module.project-services.project_id
@@ -128,67 +101,9 @@ data "google_storage_project_service_account" "gcs_account" {
   depends_on = [time_sleep.wait_after_apis]
 }
 
-# # # Create the Storage trigger
-# resource "google_storage_notification" "notification" {
-#   provider       = google
-#   bucket         = google_storage_bucket.provisioning_bucket.name
-#   payload_format = "JSON_API_V1"
-#   topic          = google_pubsub_topic.topic.id
-#   depends_on = [
-#     google_pubsub_topic_iam_binding.binding,
-#   ]
-# }
-
-# # # Create the Eventarc trigger
-# resource "google_eventarc_trigger" "trigger_pubsub_tf" {
-#   project  = module.project-services.project_id
-#   name     = "trigger-pubsub-tf"
-#   location = var.region
-#   matching_criteria {
-#     attribute = "type"
-#     value     = "google.cloud.pubsub.topic.v1.messagePublished"
-
-#   }
-#   destination {
-#     workflow = google_workflows_workflow.workflow.id
-#   }
-
-#   transport {
-#     pubsub {
-#       topic = google_pubsub_topic.topic.id
-#     }
-#   }
-#   service_account = google_service_account.eventarc_service_account.email
-
-#   labels = var.labels
-
-#   depends_on = [
-#     google_project_iam_member.eventarc_service_account_invoke_role,
-#   ]
-# }
-
-# # Set up Eventarc service account for the Trigger to execute as
-# # # Set up the Eventarc service account
-# resource "google_service_account" "eventarc_service_account" {
-#   project      = module.project-services.project_id
-#   account_id   = "eventarc-sa-${random_id.id.hex}"
-#   display_name = "Service Account for Cloud Eventarc"
-
-#   depends_on = [time_sleep.wait_after_apis]
-# }
-
-# # # Grant the Eventarc service account Workflow Invoker Access
-# resource "google_project_iam_member" "eventarc_service_account_invoke_role" {
-#   project = module.project-services.project_id
-#   role    = "roles/workflows.invoker"
-#   member  = "serviceAccount:${google_service_account.eventarc_service_account.email}"
-# }
-
 // Sleep for 120 seconds to drop start file
 resource "time_sleep" "wait_to_startfile" {
   depends_on = [
-    # google_storage_notification.notification,
-    # google_eventarc_trigger.trigger_pubsub_tf,
     google_workflows_workflow.workflow
   ]
 
