99acaba20e63e6071d26dcc2e365f3173e9b2509
Bharath KKB <bharathkrishnakb@gmail.com>
2023-11-09 23:19:44 -0600

    Apply suggestions from code review

--- modules/data_warehouse/versions.tf
+++ modules/data_warehouse/versions.tf
@@ -37,7 +37,7 @@ terraform {
       version = ">= 0.9.1"
     }
   }
-  required_version = ">= 1.3"
+  required_version = ">= 0.13"
 
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-bigquery:data_warehouse/v7.0.0"
