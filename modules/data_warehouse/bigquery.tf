6a6dbfd4ad885db62b7803d06d8742321b4cbd1d
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-11-14 11:01:44 -0500

    Add dependency for scheduled query

--- modules/data_warehouse/bigquery.tf
+++ modules/data_warehouse/bigquery.tf
@@ -381,7 +381,7 @@ resource "google_bigquery_data_transfer_config" "dts_config" {
 
   depends_on = [
     google_project_iam_member.dts_roles,
-    google_bigquery_dataset.ds_edw,
     google_service_account_iam_binding.dts_token_creator,
+    time_sleep.wait_after_workflow_execution
   ]
 }
