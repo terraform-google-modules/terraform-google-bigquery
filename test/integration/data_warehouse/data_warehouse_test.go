4aeabd201227cca252e58fc93e7ba17cbe33bce2
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-11-17 10:58:17 -0500

    Expanded integration testing support

--- test/integration/data_warehouse/data_warehouse_test.go
+++ test/integration/data_warehouse/data_warehouse_test.go
@@ -15,14 +15,18 @@
 package multiple_buckets
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/bq"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
 
+// TODO: Remove because Eventarc is no longer in use
 // Retry if these errors are encountered.
 var retryErrors = map[string]string{
 	// IAM for Eventarc service agent is eventually consistent
@@ -37,10 +41,55 @@ func TestDataWarehouse(t *testing.T) {
 
 		projectID := dwh.GetTFSetupStringOutput("project_id")
 		bucket := dwh.GetStringOutput("raw_bucket")
+		workflow := "initial-workflow"
+		datasetID := "thelook"
 
+		// Assert that the bucket is in asia-southeast1
 		bucketOP := gcloud.Runf(t, "storage buckets describe gs://%s --project %s", bucket, projectID)
-		assert.Equal("US-CENTRAL1", bucketOP.Get("location").String(), "should be in us-central1")
-		//TODO: Add additional asserts for other resources
+		assert.Equal("ASIA-SOUTHEAST1", bucketOP.Get("location").String(), "Bucket should be in asia-southeast1")
+
+		// Assert that Workflow ran successfully
+		verifyWorkflows := func() (bool, error) {
+			workflowState := gcloud.Runf(t, "workflows executions list %s --project %s --location=asia-southeast1 --limit=1", workflow, projectID).Array()
+			state := workflowState[0].Get("state").String()
+			assert.NotEqual(t, state, "FAILED")
+			if state == "SUCCEEDED" {
+				return true, nil
+			} else {
+				return false, nil
+			}
+		}
+		utils.Poll(t, verifyWorkflows, 8, 30*time.Second)
+
+		// Assert BigQuery tables & views are not empty
+		tables := []string{
+			"distribution_centers",
+			"events",
+			"inventory_items",
+			"order_items",
+			"orders",
+			"products",
+			"users",
+			"lookerstudio_report_distribution_centers",
+			"lookerstudio_report_profit",
+		}
+
+		query_template := "SELECT COUNT(*) AS count_rows FROM `%[1]s.%[2]s.%[3]s`;"
+
+		for _, table := range tables {
+			query := fmt.Sprintf(query_template, projectID, datasetID, table)
+			op := bq.Runf(t, "--project_id=%[1]s query --nouse_legacy_sql %[2]s", projectID, query)
+
+			count := op.Get("count_rows").Int()
+			assert.Greater(t, count, 0, fmt.Sprintf("Table `%s` is empty.", table))
+		}
+
+		llm_query_template := "SELECT COUNT(*) AS count_rows FROM ML.GENERATE_TEXT(MODEL `%[1]s.thelook.text_generate_model`, (with clusters AS(SELECT CONCAT('cluster', CAST(centroid_id as STRING)) as centroid, avg_spend as average_spend, count_orders as count_of_orders, days_since_order FROM (SELECT centroid_id, feature, ROUND(numerical_value, 2) as value FROM ML.CENTROIDS(MODEL `%[1]s.thelook.customer_segment_clustering`)) PIVOT (SUM(value) FOR feature IN ('avg_spend', 'count_orders', 'days_since_order')) ORDER BY centroid_id) SELECT 'Pretend you are a creative strategist, given the following clusters come up with creative brand persona and title labels for each of these clusters, and explain step by step; what would be the next marketing step for these clusters' || ' ' || clusters.centroid || ', Average Spend $' || clusters.average_spend || ', Count of orders per person ' || clusters.count_of_orders || ', Days since last order ' || clusters.days_since_order AS prompt FROM clusters), STRUCT(800 AS max_output_tokens, 0.8 AS temperature, 40 AS top_k, 0.8 AS top_p, TRUE AS flatten_json_output));"
+		query := fmt.Sprintf(llm_query_template, projectID)
+		llm_op := bq.Runf(t, "--project_id=%[1]s query --nouse_legacy_sql %[2]s", projectID, query)
+
+		llm_count := llm_op.Get("count_rows").Int()
+		assert.Greater(t, llm_count, 0, "The ML.GENERATE_TEXT() query failed")
 	})
 	dwh.Test()
 }
