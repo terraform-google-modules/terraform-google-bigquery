6be6bbe58afd4fdd64a875831ca51372e7f2d6d4
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-11-30 11:44:09 -0500

    Diagnosing query comparison issues

--- test/integration/data_warehouse/data_warehouse_test.go
+++ test/integration/data_warehouse/data_warehouse_test.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -96,13 +97,19 @@ func TestDataWarehouse(t *testing.T) {
 			query := fmt.Sprintf(query_template, projectID, table)
 			op := bq.Runf(t, "--project_id=%[1]s --headless=true query --nouse_legacy_sql %[2]s", projectID, query)
 
-			count, ok := op.Get("count_rows").Int()
-			if ok{
+			count := op.Get("count_rows").Int()
+			count_type := reflect.TypeOf(count).Kind()
+
+			if count_type == reflect.Int {
 				assert.Greater(t, count, 0, fmt.Sprintf("Table `%s` is empty.", table))
 			} else {
-				log.Printf("[ERROR] in type conversion: '%+v' is not an integer", op.Get("count_rows"))
+				if count_type != reflect.Int {
+					assert.Greater(t, int(count), 0, fmt.Sprintf("Table `%s` is empty.", table))
+				} else {
+					log.Printf("[ERROR] in type conversion: '%+v' is not an integer or string", op.Get("count_rows"))
+					}
+				}
 			}
-		}
 
 		// Assert BigQuery connection to Vertex GenAI was successfully created and works as expected
 		llm_query_template := "SELECT COUNT(*) AS count_rows FROM ML.GENERATE_TEXT(MODEL `%[1]s.thelook.text_generate_model`, (with clusters AS(SELECT CONCAT('cluster', CAST(centroid_id as STRING)) as centroid, avg_spend as average_spend, count_orders as count_of_orders, days_since_order FROM (SELECT centroid_id, feature, ROUND(numerical_value, 2) as value FROM ML.CENTROIDS(MODEL `%[1]s.thelook.customer_segment_clustering`)) PIVOT (SUM(value) FOR feature IN ('avg_spend', 'count_orders', 'days_since_order')) ORDER BY centroid_id) SELECT 'Pretend you are a creative strategist, given the following clusters come up with creative brand persona and title labels for each of these clusters, and explain step by step; what would be the next marketing step for these clusters' || ' ' || clusters.centroid || ', Average Spend $' || clusters.average_spend || ', Count of orders per person ' || clusters.count_of_orders || ', Days since last order ' || clusters.days_since_order AS prompt FROM clusters), STRUCT(800 AS max_output_tokens, 0.8 AS temperature, 40 AS top_k, 0.8 AS top_p, TRUE AS flatten_json_output));"
