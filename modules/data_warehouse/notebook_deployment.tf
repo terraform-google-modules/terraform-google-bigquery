5018c723e6e82199d83fcb582c07000d69409cd5
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-17 14:24:28 -0500

    defining dependencies

--- modules/data_warehouse/notebook_deployment.tf
+++ modules/data_warehouse/notebook_deployment.tf
@@ -101,11 +101,13 @@ resource "google_service_account_iam_member" "workflow_auth_function" {
 }
 
 # Create a Dataform repo to host notebooks
-resource "google_dataform_repository" "notebook-repo" {
+resource "google_dataform_repository" "notebook_repo" {
   provider = google-beta
   project = module.project-services.project_id
   region = var.region
   name = "thelook-learning-resources"
+
+  depends_on = [ time_sleep.wait_after_apis ]
 }
 
 # Create and deploy a Cloud Function to deploy notebooks
@@ -141,9 +143,9 @@ resource "google_cloudfunctions2_function" "notebook_deploy_function" {
     environment_variables = {
       "PROJECT_ID" : module.project-services.project_id,
     "REGION" : var.region
-    "REPO_ID" : google_dataform_repository.notebook-repo.id }
+    "REPO_ID" : google_dataform_repository.notebook_repo.id }
   }
-  depends_on = [time_sleep.wait_after_apis, google_project_iam_member.function_manage_roles]
+  depends_on = [time_sleep.wait_after_apis, google_project_iam_member.function_manage_roles, google_dataform_repository.notebook_repo]
 }
 
 resource "time_sleep" "wait_after_function" {
