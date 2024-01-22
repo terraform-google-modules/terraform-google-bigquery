532e8fdaaf0e6ed2d0a4497d934dd76f4e802fb4
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-22 14:53:49 -0500

    Formatting

--- modules/data_warehouse/notebook_deployment.tf
+++ modules/data_warehouse/notebook_deployment.tf
@@ -35,9 +35,9 @@ resource "local_file" "notebooks" {
 
 # Create the notebook runtime to save time after deployment
 resource "google_notebooks_runtime" "notebook_runtime" {
-  name = "notebook-runtime"
+  name     = "notebook-runtime"
   location = var.region
-  project = module.project-services.project_id
+  project  = module.project-services.project_id
   virtual_machine {
     virtual_machine_config {
       machine_type = "e2-standard-4"
@@ -45,14 +45,14 @@ resource "google_notebooks_runtime" "notebook_runtime" {
         auto_delete = var.runtime_auto_delete
         initialize_params {
           disk_size_gb = "20"
-          disk_type = "PD_STANDARD"
+          disk_type    = "PD_STANDARD"
         }
       }
       labels = var.labels
     }
   }
   software_config {
-    idle_shutdown = true
+    idle_shutdown         = true
     idle_shutdown_timeout = "90m"
   }
 }
--- modules/data_warehouse/variables.tf
+++ modules/data_warehouse/variables.tf
@@ -68,9 +68,9 @@ variable "deletion_protection" {
 }
 
 variable "runtime_auto_delete" {
-  type = string
+  type        = string
   description = "Specifies whether the disk will be auto-deleted when the notebook runtime VM instance is deleted"
-  default = true
+  default     = true
 }
 
 
