2ca4007488712be07d62326a53820285c0f883ca
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-26 11:57:09 -0500

    Define local version requirements

--- modules/data_warehouse/versions.tf
+++ modules/data_warehouse/versions.tf
@@ -40,6 +40,10 @@ terraform {
       source  = "hashicorp/http"
       version = ">= 2"
     }
+    local = {
+      source = "hashicorp/local"
+      version = ">=2.4"
+    }
   }
   required_version = ">= 0.13"
 
