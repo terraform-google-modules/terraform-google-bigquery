88cd487b43a5a3b64d84302bc51097c921224537
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-01-30 10:22:36 -0500

    Formatting

--- modules/data_warehouse/variables.tf
+++ modules/data_warehouse/variables.tf
@@ -23,7 +23,8 @@ variable "region" {
   type        = string
   description = "Google Cloud Region"
 
-  /* This variable list assumes you are using the same region for both Dataform and all other assets.
+/**
+ * This variable list assumes you are using the same region for both Dataform and all other assets.
  * If you want to deploy your Dataform respositories in a different region, set the default value
  * for var.dataform_region to one of the regions in the Dataform validation list.
  * You can then set this variable value to any of the following:
