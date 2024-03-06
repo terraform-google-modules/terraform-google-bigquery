8ccc96b897f8f25163c0d3f4a4444d4f9cd36034
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2024-03-06 13:35:22 -0500

    cleanup

--- modules/data_warehouse/workflows.tf
+++ modules/data_warehouse/workflows.tf
@@ -120,7 +120,8 @@ data "http" "call_workflows_state_1" {
 }
 
 locals {
-  json_workflow_state = jsondecode(data.http.call_workflows_state_1).executions[0].state
+  json_workflow_result = jsondecode(data.http.call_workflows_state_1)
+  json_workflow_state = local.json_workflow_result.executions[0].state
   depends_on = [time_sleep.wait_after_workflow_execution, data.http.call_workflows_state_1]
 }
 
