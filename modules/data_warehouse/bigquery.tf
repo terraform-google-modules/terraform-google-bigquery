beca40af88818a0c6cb01df4a205d4cc5ae67e27
bradmiro <bmiro@google.com>
2026-06-10 23:05:30 +0000

    Update Remote GenAI Model connection reference syntax to use fully qualified resource ID as requested by the user, testing parser compatibility across integration CI control planes

--- modules/data_warehouse/bigquery.tf
+++ modules/data_warehouse/bigquery.tf
@@ -343,7 +343,7 @@ resource "google_bigquery_job" "run_sp_bigqueryml_generate_create" {
     query = templatefile("${path.module}/src/sql/sp_bigqueryml_generate_create.sql", {
       project_id    = module.project-services.project_id,
       dataset_id    = google_bigquery_dataset.ds_edw.dataset_id,
-      connection_id = "${var.region}.${google_bigquery_connection.vertex_ai_connection.connection_id}",
+      connection_id = google_bigquery_connection.vertex_ai_connection.name,
       model_name    = var.text_generation_model_name,
       region        = var.region
     })
